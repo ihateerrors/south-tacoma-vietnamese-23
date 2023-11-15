01489f1b19915f7f9bdb961686c476df07629364
CarlosPrieto <carlosprieto66@icloud.com>
2023-11-14 18:10:23 -0800

    first round of edits

--- js/main.js
+++ js/main.js
@@ -74,8 +74,5 @@ $(window).scroll(function () {
 
 // Tooltip functionality
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
